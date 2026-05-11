0da22bccd8922d5ddeffa9f90ef219f332c79e85
riddhi22082004 <sharmariddhi022@gmail.com>
2026-05-12 00:52:00 +0530

    fix analyze endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,43 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from fastapi.middleware.cors import (
+    CORSMiddleware
+)
+
+from app.api.routes import router
+
+
+app = FastAPI(
+    title="AI Website Intelligence API"
+)
+
+
+# CORS
+
+app.add_middleware(
+
+    CORSMiddleware,
+
+    allow_origins=["*"],
+
+    allow_credentials=True,
+
+    allow_methods=["*"],
+
+    allow_headers=["*"],
+)
+
+
+# ROUTES
+
+app.include_router(router)
+
 
 @app.get("/")
-def home():
-    return {"message": "running"}
+def root():
+
+    return {
+
+        "message":
+        "AI Website Intelligence API Running"
+    }
